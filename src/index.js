b7909ab239ccb2312e6d7c30a5df725c87bcfd85
jaydip <jaydipbsatani@gmail.com>
2025-06-08 11:14:44 +0530

    inital commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import express from "express";
+import "dotenv/config";
+const app = express();
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
