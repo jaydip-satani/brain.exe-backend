91d99ddb1fe1428d30440c015497b8b288bbb257
jaydip <jaydipbsatani@gmail.com>
2025-06-11 15:52:22 +0530

    logger added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import authRoutes from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
+import { logger } from "./utils/logger.js";
 const app = express();
 const PORT = process.env.PORT;
 
@@ -11,4 +12,4 @@ app.get("/", (req, res) => {
 });
 app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes);
-app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
+app.listen(PORT, () => logger.info(`Server started successfully on ${PORT}`));
